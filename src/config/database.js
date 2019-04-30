a7457f51f319ef0fc72b3b9a1849b1551072c42e
cristhian caldas <c.caldas.m@gmail.com>
2019-04-29 22:26:34 -0500

    optimizando el codigo

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,6 @@
 const mongoose=require('mongoose');
 
 const URI = 'mongodb+srv://apirest:apirest@cluster0-rqsae.mongodb.net/tallerninja?';
-//mongodb+srv://apirest:<password>@cluster0-rqsae.mongodb.net/test?retryWrites=true
 
 mongoose.connect(URI)
 .then(db=>console.log('DB CONNECT'))
